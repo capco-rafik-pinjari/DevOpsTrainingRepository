1d9005b623d14813badb5bd2214463d97ed95538
Rafik Pinjari <she.rafik.pinjari@capco.com>
2019-09-25 12:04:52 +0530

    baseline code

---
+++ src/main/java/com/java/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.java.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "success";
+	}
+
+}
