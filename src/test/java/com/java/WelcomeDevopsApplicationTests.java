918d0931196700b3237d17fdd0fbb27033760caf
Rafik Pinjari <she.rafik.pinjari@capco.com>
2019-09-26 09:39:14 +0530

    added test case

--- src/test/java/com/java/WelcomeDevopsApplicationTests.java
+++ src/test/java/com/java/WelcomeDevopsApplicationTests.java
@@ -1,16 +1,25 @@
 package com.java;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.java.service.WelcomeService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WelcomeDevopsApplicationTests {
+	
+	@Autowired
+	WelcomeService welcomeservice;
 
 	@Test
 	public void contextLoads() {
+		assertEquals("success", welcomeservice.welcome());
 	}
 
 }
